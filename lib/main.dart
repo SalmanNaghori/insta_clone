c249cd4ba7212aa8975386392410c3993cd1c4d4
SalmanNaghori <salmannaghori@gmail.com>
2023-11-26 17:32:36 +0530

    make a responsive screen for mobile and web also

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:insta_clone/utils/app_colors.dart';
+import 'package:insta_clone/responsive/mobile_screen_layout.dart';
+import 'package:insta_clone/responsive/reponsive_layout_screen.dart';
+import 'package:insta_clone/responsive/web_screen_layout.dart';
+import 'package:insta_clone/constant/app_colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +14,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Insta Clone',
-      theme: ThemeData.dark(useMaterial3: true)
-          .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
-      home: Scaffold(
-        body: Text("data"),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Insta Clone',
+        theme: ThemeData.dark(useMaterial3: true)
+            .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+        home: const ResponsiveLayout(
+          mobileScreen: MobileScreen(),
+          webScreen: WebScreen(),
+        ));
   }
 }
--- lib/responsive/reponsive_layout_screen.dart
+++ lib/responsive/reponsive_layout_screen.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:insta_clone/utils/dimensions.dart';
 
+class ResponsiveLayout extends StatelessWidget {
+  final Widget mobileScreen;
+  final Widget webScreen;
+  const ResponsiveLayout({
+    Key? key,
+    required this.mobileScreen,
+    required this.webScreen,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      // TODO:web screen layout
+      if (constraints.maxWidth > webScreenSize) {
+        return webScreen;
+      }
+      // TODO: Mobile screen layout
+      else {
+        return mobileScreen;
+      }
+    });
+  }
+}
